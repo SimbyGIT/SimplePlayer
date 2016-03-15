b7cf5ecb998124f7f07cfd483e469a5de4aa41f9
SimbyOS <simbyos@gmail.com>
2016-03-15 19:26:32 +0300

    Revert "Revert "Базовые функции для формы Video Самодельный AutoSize формы при Resize""
    This reverts commit 79ed24f1a27e4196f5822a60b7b2d0eb078fb35a.

--- Simple Player/Download.Designer.vb
+++ Simple Player/Download.Designer.vb
@@ -28,6 +28,7 @@ Partial Class Download
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
         Me.SuspendLayout
         '
         'ButtonDownload
@@ -71,11 +72,22 @@ Partial Class Download
         Me.Label3.Size = New System.Drawing.Size(0, 13)
         Me.Label3.TabIndex = 4
         '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = true
+        Me.LinkLabel1.Location = New System.Drawing.Point(242, 63)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(115, 13)
+        Me.LinkLabel1.TabIndex = 5
+        Me.LinkLabel1.TabStop = true
+        Me.LinkLabel1.Text = "Параметры загрузки"
+        '
         'Download
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(368, 85)
+        Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -95,4 +107,5 @@ End Sub
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
 End Class
--- Simple Player/Download.vb
+++ Simple Player/Download.vb
@@ -32,4 +32,8 @@ Public Class Download
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
     End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+
+    End Sub
 End Class
